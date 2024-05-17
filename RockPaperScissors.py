a371f24a8e25e96cd70d52cb9de81af9f96c9757
Matublaq <mati.blaquier@gmail.com>
2024-05-17 07:35:19 +0100

    none

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -26,20 +26,22 @@ while((Player_points and PlayerAI_points) < maxPoint):
     while((Player!="Rock")and(Player!="Paper")and(Player!="Scissors")):
         Player = input("Rock, paper or scissors? \n")
         Player = Player.capitalize()
+    
+    print('\n', Player, ' vs ', PlayerAI)
 #**********************************************Begins game**********************************************************************
     if(Player==PlayerAI):
-        print("They both took out", Player)
+        print("They both took out", Player, '\n')
         Player_points += 1
         PlayerAI_points += 1
-        print(Player_name, ":", Player_points, "\nPlayerAI: ", PlayerAI_points)
+        print(Player_name, ":", Player_points, "\nPlayerAI: ", PlayerAI_points, '\n')
     elif(((Player=="Rock")and(PlayerAI=="Paper"))or((Player=="Paper") and (PlayerAI=="Scissors"))or((Player=="Scissors") and (PlayerAI=="Rock"))):
         PlayerAI_points += 1
-        print("PlayerAI win the point.")
-        print(Player_name, ":", Player_points, "\nPlayerAI: ", PlayerAI_points)
+        print("PlayerAI win the point.", '\n')
+        print(Player_name, ":", Player_points, "\nPlayerAI: ", PlayerAI_points, '\n')
     elif(((PlayerAI=="Rock")and(Player=="Paper"))or((PlayerAI=="Paper") and (Player=="Scissors"))or((PlayerAI=="Scissors") and (Player=="Rock"))):
         Player_points += 1
-        print(Player_name, " win the point.")
-        print(Player_name, ":", Player_points, "\nPlayerAI: ", PlayerAI_points)
+        print(Player_name, " win the point.", '\n')
+        print(Player_name, ":", Player_points, "\nPlayerAI: ", PlayerAI_points, '\n')
     else:
         print("Error 2")
     
@@ -47,7 +49,7 @@ while((Player_points and PlayerAI_points) < maxPoint):
 
 
 if((Player_points==maxPoint) and (PlayerAI_points==maxPoint)):
-    print("Tie")
+    print("Tie", '\n')
 
 elif(Player_points==maxPoint):
     print(Player_name.upper(), " WIN!")
